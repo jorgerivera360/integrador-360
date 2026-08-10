d8d20190f6ef32429ce996a416b17118a493ca66
jorgerivera360 <jorge.rivera@360Software.com.co>
2026-08-10 11:05:53 -0500

    Test conexión bd

--- test_conexion.py
+++ test_conexion.py
@@ -8,20 +8,19 @@ config = ConfigLoader("pruebaws").load_config()
 config["client_id"] = "pruebaws"
 connector = SiesaEnterprise(config)
 
-status, data = connector.get(
-    endpoint="EjecutarConsultaXML",
-    params={"sql": """
-        SET QUOTED_IDENTIFIER OFF;
-        SELECT TOP 5
-            f120_id AS referencia,
-            REPLACE(ISNULL(f120_descripcion, 'SIN DESCRIPCION'), CHAR(39), '') AS descripcion,
-            ISNULL(TRIM(f120_id_unidad_inventario), 'UNID') AS unidad
-        FROM t120_mc_items
-        WHERE f120_id_cia = 1
-        ORDER BY f120_id ASC;
-        SET QUOTED_IDENTIFIER ON;
-    """}
+sql = (
+    'SET QUOTED_IDENTIFIER OFF;'
+    ' SELECT TOP 5'
+    ' f120_id AS referencia,'
+    ' ISNULL(f120_descripcion, "") AS descripcion,'
+    ' ISNULL(TRIM(f120_id_unidad_inventario), "UNID") AS unidad'
+    ' FROM t120_mc_items'
+    ' WHERE f120_id_cia = 1'
+    ' ORDER BY f120_id ASC;'
+    ' SET QUOTED_IDENTIFIER ON;'
 )
+
+status, data = connector.get(endpoint="EjecutarConsultaXML", params={"sql": sql})
 print("Status:", status)
 print("Registros:", len(data) if status else data)
 if status and data:
