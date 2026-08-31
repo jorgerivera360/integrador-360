31ba178c6becd9b8fdbf34be80fff984d07fd2ce
jorgerivera360 <jorge.rivera@360Software.com.co>
2026-08-31 14:20:19 -0500

    Ajuste arranque ordenado

--- scheduler/runner.py
+++ scheduler/runner.py
@@ -196,7 +196,8 @@ class IntegradorScheduler:
         )
 
         for flow in self.flows:
-            self._run_with_retry(flow)
+            if flow.get("schedule_cron"):
+                self._run_with_retry(flow)
 
         self.logger.info("Scheduler | Arranque ordenado completado")
 
